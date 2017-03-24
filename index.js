0ff636c1e0a2fa3ce064e9375c3d463f02b31c44
Lee SeungHun <leengine90@gmail.com>
2017-03-24 10:11:25 +0900

    init

---
+++ index.js
@@ -0,0 +1,36 @@
+var argv = require('minimist')(process.argv.slice(2)),
+	neo4j = require('neo4j-driver').v1,
+	async = require('async'),
+	fs = require('fs'),
+	config = {
+		url: argv.url? argv.url: 'bolt://localhost:7687',
+		user: argv.user? argv.user: 'neo4j',
+		pass: argv.pass? argv.pass: 'neo4j',
+		filename: argv._[0]? argv._[0]: 'cypher.cql'   
+	},
+	driver = new neo4j.driver(config.url, neo4j.auth.basic(config.user, config.pass)),
+	cqls = fs.readFileSync(`./${config.filename}`, 'utf-8'),
+	fns = cqls.split(';')
+			.map((cql) => (
+				(callback)=>{
+					var session = driver.session();
+					console.log(cql);
+					session.run(cql)
+						.then(function() {
+							session.close();
+							callback(null, cql);
+						}).catch(function(err) {
+							session.close();
+							callback(err, cql)
+						});
+				})
+			)
+
+async.series(fns, function(err, result) {
+	if(err) {
+		console.log(err);
+	}
+	console.log(result);
+});
+
+
